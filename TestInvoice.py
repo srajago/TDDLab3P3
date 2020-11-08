425e9ffc50aed81ef2bfdd6979aab7ec7f49cd08
srajago <srajago7@uncc.edu>
2020-11-07 19:05:11 -0500

    commit with changes

--- TestInvoice.py
+++ TestInvoice.py
@@ -8,8 +8,20 @@ def products():
                 'Notebook': {'qnt': 5, 'unit_price': 7.5, 'discount': 10}}
     return products
 
-
-def test_CanCalculateTotalImpurePrice(products):
+@pytest.fixture()
+def invoice():
     invoice = Invoice()
+    return invoice
+
+
+def test_CanCalculateTotalImpurePrice(invoice, products):
     invoice.totalImpurePrice(products)
     assert invoice.totalImpurePrice(products) == 75
+
+def test_CanCalculateTotalDiscount(invoice, products):
+    invoice.totalDiscount(products)
+    assert invoice.totalDiscount(products) == 5.62
+
+def test_CanCalculateTotalPurePrice(invoice, products):
+    invoice.totalPurePrice(products)
+    assert invoice.totalPurePrice(products) == 69.38
--- main.py
+++ main.py
@@ -1,16 +1,19 @@
- # This is a sample Python script.
+from Invoice import Invoice
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+products = {}
+total_amount = 0
+repeat = ''
+while True:
+    product = input('What is your product : ')
+    unit_price = Invoice().inputNumber("Please enter unit price : ")
+    qnt = Invoice().inputNumber("Please enter quantity of product :")
+    discount = Invoice().inputNumber("Discount percent (%) : ")
+    repeat = Invoice().inputAnswer("Another product? (y,n) : ")
+    result = Invoice().addProduct(qnt, unit_price, discount)
+    products[product] = result
+    if repeat == "n":
+        break
 
+total_amount = Invoice().totalPurePrice(products)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Your total pure price is : ", total_amount)
